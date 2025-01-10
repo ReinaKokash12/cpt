bc10cd1c56f4ad5ebeb2ad01583fd260fa4c2572
Rei <rkokash1211@gmail.com>
2025-01-10 11:43:57 -0500

    Add files via upload

---
+++ TicketSystem.java
@@ -0,0 +1,37 @@
+import java.utill.Scanner;
+import java.utill.ArrayList;
+import java.utill.List;
+
+
+class Reservation {
+    private int id;
+    private String name;
+    private String date;
+    private int numberOfGuests;
+
+
+    public Reservation(int id, String name, String date, int numberOfGuests) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.numberOfGuests = numberOfGuests;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public String getDate() {
+        return date;
+    }
+
+
+    public int getNumberOfGuests
+}
